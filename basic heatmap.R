c32e1cbcc5d8707a4d6b45f4880d4e1be0b7732c
linhuiwen <62541713+linhuiwen@users.noreply.github.com>
2020-03-28 14:01:19 +0800

    Update basic heatmap.R

--- basic heatmap.R
+++ basic heatmap.R
@@ -3,10 +3,13 @@ library(leaflet.extras)
 library(dplyr)
 library(htmlwidgets)
 
+df <- read.csv("CA.csv")
+sample.df <- sample_n(df, 60000)
+
 m <- leaflet() %>% 
   setView(lat = 36.7783, lng = -119.4179, zoom = 6) %>% 
   addProviderTiles(providers$OpenStreetMap.Mapnik) %>%
-  addWebGLHeatmap(data = sam.df, 
+  addWebGLHeatmap(data = sample.df, 
                   lng = ~Start_Lng, 
                   lat = ~Start_Lat, 
                   opacity = 0.5)
