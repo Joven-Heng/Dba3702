ee9a1a25f3714fd2249ab58a264104b554190f2a
mengheng97 <62541836+mengheng97@users.noreply.github.com>
2020-04-17 22:30:44 +0800

    Update 1-10.R

--- 1-10.R
+++ 1-10.R
@@ -168,6 +168,53 @@ sidebar <- dashboardSidebar(
 )
 
 body <- dashboardBody(
+  tags$head(tags$style(HTML('
+                                /* logo */
+                                .skin-blue .main-header .logo {
+                                background-color: #233C67;
+                                }
+
+                                /* logo when hovered */
+                                .skin-blue .main-header .logo:hover {
+                                background-color: #233C67;
+                                }
+
+                                /* navbar (rest of the header) */
+                                .skin-blue .main-header .navbar {
+                                background-color: #233C67;
+                                }
+
+                                /* main sidebar */
+                                .skin-blue .main-sidebar {
+                                background-color: #000000;
+                                }
+
+                                /* active selected tab in the sidebarmenu */
+                                .skin-blue .main-sidebar .sidebar .sidebar-menu .active a{
+                                background-color: #FFFFFF;
+                                }
+
+                                /* other links in the sidebarmenu */
+                                .skin-blue .main-sidebar .sidebar .sidebar-menu a{
+                                background-color: #FFFFFF;
+                                color: #233C67;
+                                }
+
+                                /* other links in the sidebarmenu when hovered */
+                                .skin-blue .main-sidebar .sidebar .sidebar-menu a:hover{
+                                background-color: #233C67;
+                                }
+                                /* toggle button when hovered  */
+                                .skin-blue .main-header .navbar .sidebar-toggle:hover{
+                                background-color: #233C67;
+                                }
+
+                                /* body */
+                                .content-wrapper, .right-side {
+                                background-color: #808080;
+                                }
+
+                                '))),
   
   tabItems(
     # Tab 1 content
